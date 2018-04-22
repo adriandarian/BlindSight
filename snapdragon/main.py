ecfc465973b52a5dd65327be3c44719ee699e0f3
Jack Kolb <jackhunterkolb@gmail.com>
2018-04-22 18:32:42 +0000

    added request

--- snapdragon/main.py
+++ snapdragon/main.py
@@ -229,8 +229,8 @@ def primary_control():
 
         distance_ft = distance_cm / 2.54 / 12  # converts the distance from centimeters to feet
 #        inverse_distance_ft = 30 - distance_ft  # gets the inverse_distance (30ft = 0ft, 20ft = 10ft, 10ft = 20ft)
-
         
+        sendPoint((5-int(point))*12, distance_ft)
 
         if int(distance_ft) > 10:
             GPIO.output(vibrator_pins[str(point)], 0)
