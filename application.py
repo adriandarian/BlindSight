e2a9f37d7174eeeeab3dc5e755e827b3f0ed1c39
Jack Kolb <jackhunterkolb@gmail.com>
2018-04-22 04:25:36 -0700

    fixed the application.py

--- application.py
+++ application.py
@@ -1,23 +1,26 @@
-from flask import Flask
+import os
+from flask import Flask,redirect
 
-#print a nice greeting.
-def say_hello(username = "World"):
-	return '<p>Hello %s!</p>\n' % username
+try:
 
-#some bits of text for the page.
-header_text = '''<html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''<p><em>Hint</em>: This is a RESTful web service! Append a username to the URL (for example: <code>/THelonious</code>) to say hello to someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+	app = Flask(_name_)
 
-#EB looks for an 'application' callable by default.
-application = Flask(__name__)
+	@app.route('/')
+	def hello():
+	    return redirect("https://adriandarian.github.io/blindsight", 
+code=302)
 
-#add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text + say_hello() + instructions + footer_text))
+	@app.route('/live', methods=['POST'])
+	def info():
+		req_data = request.get_json()
+		print req_data
 
-#run the app
-if __name__ == "__main__":
-	#setting debug to True enables debug output. This line should be removed before deploying a production app.
-	application.debug = True
-	appliation.run()
+	if _name_ == '_main_':
+	    # Bind to PORT if defined, otherwise default to 5000.
+	    port = int(os.environ.get('PORT', 5000))
+	    app.run(host='127.0.0.1', port=port)
+
+
+except Exception as e:
+	with open("logg.txt", "w") as logfile:
+		logfile.write(str(e.what()))
